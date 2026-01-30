9a145de8c22ac173affdbf4fedcd93e1332e167d
Eesha <76590268+EESHAK02@users.noreply.github.com>
2026-01-29 16:31:33 -0800

    Updating template

--- config.py
+++ config.py
@@ -8,8 +8,4 @@ MODEL_NAME = "llama3.1:latest"
 # TEMPERATURE = 0.2
 # MAX_TOKENS = 512
 
-# # Prompt templates (to be filled later)
-# INTENT_PROMPT = ""
-# CLARIFICATION_PROMPT = ""
-# PLANNING_PROMPT = ""
-# REVISION_PROMPT = ""
+# can set common prompts here
